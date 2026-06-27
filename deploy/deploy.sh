c0a91295730fd1d8badc1339bc956e6654709c99
robert-mccausland <robert.mccausland10@gmail.com>
2026-06-27 19:38:21 +0100

    fix: dont use long for depend on as my server has old docker version

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -11,5 +11,11 @@ alias docker-compose='docker run --rm \
 
 cd /home/apps/janitor-bot
 
+
+# Hack to fix volume permissions because docker-compose doesn't support setting the user for volumes
+docker volume create --driver=local janitor-bot_janitor-bot-data
+docker run --rm -v janitor-bot_janitor-bot-data:/var/lib/janitor-bot/data \
+    alpine:3.22 sh -c "mkdir -p /var/lib/janitor-bot/data && chown -R 1000:1000 /var/lib/janitor-bot/data"
+
 docker-compose down
 docker-compose up -d --force-recreate
